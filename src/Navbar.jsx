0feb92e40a0905c9b55ddd36bb9d3b589f14bfb6
Bhargavi <krishnabhargavi01@gmail.com>
2026-08-01 19:28:36 +0530

    Updated resume

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -13,7 +13,7 @@ function Navbar() {
           <li><a href="#projects">Projects</a></li>
           <li><a href="#contact">Contact</a></li>
         </ul>
-        <a href="/Katikam_Bhargavi!.pdf" className="resume-btn">Resume</a>
+        <a href="/KatikamBhargavi-Resume.pdf" className="resume-btn">Resume</a>
     </div>
     </>
   )
